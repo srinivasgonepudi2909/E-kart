c59bb341edb66bf0ddefe03db146db8bd3fcf8f5
srinivas <srigonepudi@gmail.com>
2025-08-03 19:09:05 +0530

    updated

--- front-end/src/components/Home.js
+++ front-end/src/components/Home.js
@@ -1,3 +1,4 @@
+
 import { Link } from "react-router-dom";
 import "./Home.css";
 
@@ -7,9 +8,16 @@ export default function Home() {
   ];
 
   const products = [
-    { id: 1, name: "iPhone 15", price: "₹75,000", image: "https://m.media-amazon.com/images/I/81+GIkwQbNL._SX679_.jpg" },
-    { id: 2, name: "MacBook Pro", price: "₹1,20,000", image: "https://m.media-amazon.com/images/I/71L2iBSyyOL._SL1500_.jpg" },
-    { id: 3, name: "Samsung Galaxy", price: "₹65,000", image: "https://m.media-amazon.com/images/I/81ZSn2rk9WL._SL1500_.jpg" },
+    { id: 1, name: "iPhone 16 Pro", price: "₹1,29,000", image: "/images/mobiles/iphone1.jpg" },
+    { id: 2, name: "iPhone 16", price: "₹1,19,000", image: "/images/mobiles/iphone2.jpg" },
+    { id: 3, name: "Samsung S24 Ultra", price: "₹1,34,000", image: "/images/mobiles/samsung.webp" },
+    { id: 4, name: "Samsung Galaxy", price: "₹65,000", image: "/images/mobiles/samsung2.webp" },
+    { id: 5, name: "OnePlus 13", price: "₹58,000", image: "/images/mobiles/oneplus-13-white.png" },
+    { id: 6, name: "OnePlus Nord 4", price: "₹39,000", image: "/images/mobiles/OnePlus-Nord-4-5G-1-1.webp" },
+    { id: 7, name: "OnePlus Nord 4 Green", price: "₹40,000", image: "/images/mobiles/oneplus-nord-4-oasis-green-01.jpg" },
+    { id: 8, name: "POCO X4 Pro", price: "₹23,000", image: "/images/mobiles/poco-x4-pro-5g-yellow-8gb-128gb-store.webp" },
+    { id: 9, name: "Oppo Reno", price: "₹27,000", image: "/images/mobiles/reno-opppo.jpg" },
+    { id: 10, name: "Vivo V30", price: "₹32,000", image: "/images/mobiles/vivo.webp" }
   ];
 
   return (
